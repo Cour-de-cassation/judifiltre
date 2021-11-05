e0c503d8178a4afce23bf76124e3122aa6a533fb
BenoitSerrano <benoit.serrano10@gmail.com>
2021-11-05 15:29:26 +0100

    add Docker to judifiltre

--- backend/src/api/buildApi.ts
+++ backend/src/api/buildApi.ts
@@ -1,10 +1,21 @@
 import { Express } from "express";
+import { MongoClient } from "mongodb";
+
 import { buildRoutes } from "./routes";
 
 export { buildApi };
 
-function buildApi(app: Express) {
+async function buildApi(app: Express) {
   const routes = buildRoutes();
 
   app.use("/judifiltre/api", routes);
+
+  await setupMongo();
+}
+
+async function setupMongo() {
+  console.log(`Loading the Mongo database...`);
+  const client = new MongoClient(`mongodb://localhost:27017`);
+  await client.connect();
+  console.log(`MongoDB ready!`);
 }
