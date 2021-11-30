cfd8f8062e1fdbcf70af01f8f15cf04d02fb04ab
BenoitSerrano <benoit.serrano10@gmail.com>
2021-11-30 15:55:52 +0100

    change way to display lerkRequest

--- client/src/pages/Assessor/PublicityInfoRow.tsx
+++ client/src/pages/Assessor/PublicityInfoRow.tsx
@@ -13,7 +13,11 @@ function PublicityInfoRow(props: { publicityInfo: publicityInfoType }) {
       <div>{props.publicityInfo.sourceId}</div>
       <div>{props.publicityInfo.jurisdiction}</div>
       <div>{props.publicityInfo.fieldCode}</div>
-      <div>{props.publicityInfo.publicity.clerkRequest || "-"}</div>
+      <div>
+        {props.publicityInfo.publicity.clerkRequest === "unspecified"
+          ? "-"
+          : props.publicityInfo.publicity.clerkRequest}
+      </div>
       <div>{props.publicityInfo.publicity.assessment}</div>
     </div>
   );
