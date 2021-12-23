5dd8d65e78dc4a0cc00c57d63dbb366a81d93895
antoinefa <fabien.antoine@m4x.org>
2021-12-23 10:24:37 +0100

    removes usage of body-parser, not needed since express 4.16.x

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,13 @@
 import "./config.ts";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import { buildApi } from "./api";
 
 const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 buildApi(app);
 
