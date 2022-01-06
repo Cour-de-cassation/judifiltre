f754f832f49a767547b652af2ce8dcbb8516ff70
antoinefa <fabien.antoine@m4x.org>
2022-01-06 15:41:38 +0100

    blank password of mongo uri in log

--- backend/src/app/setup.ts
+++ backend/src/app/setup.ts
@@ -1,17 +1,21 @@
 import { mongo, dbConfigs, buildMongo } from "../lib";
 
-export { setupMongo };
+export { setupMongo, connected };
+
+let connected = false;
 
 async function setupMongo() {
   console.log(`Setting up Mongo...`);
   await Promise.all(
     dbConfigs.map(async ({ dbName, url }) => {
-      console.log(`Loading ${dbName} database for url ${url}...`);
+      const secUrl = url.replace(/\/\/(.*):.*@/, '//$1:***@');
+      console.log(`Loading ${dbName} database for url ${secUrl}...`);
       mongo[dbName] = buildMongo();
       await mongo[dbName].initialize({ dbName, url});
 
       console.log(`${dbName} ready!`);
     })
   );
+  connected = true;
   console.log(`MongoDB ready!`);
 }
