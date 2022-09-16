c7495ca89bd06a9893ebaf4380653113e1be5520
antoinefa <fabien.antoine@m4x.org>
2022-09-15 20:53:53 -0400

    fix buggy deploy_k8s_local.sh

--- scripts/deploy_k8s_local.sh
+++ scripts/deploy_k8s_local.sh
@@ -1,11 +1,14 @@
 for APP_ID in judifiltre-backend judifiltre-client;do
     export eval $(cat .env-sample-${APP_ID});
-    if [ "${APP_ID}" == "judifiltre-client"]; then
+    if [ "${APP_ID}" == "judifiltre-client" ]; then
         yarn buildClient;
+    else
+        yarn buildBackend;
     fi
-    docker stop ${APP_ID};docker rm ${APP_ID};docker image rm ${DOCKER_USERNAME}/${APP_ID}:$(./scripts/version.sh);
+    (docker stop ${APP_ID};docker rm ${APP_ID}) >/dev/null 2>&1;
+    docker image rm ${DOCKER_USERNAME}/${APP_ID}:$(./scripts/version.sh) > /dev/null 2>&1;
     export DOCKER_TARGET=${APP_ID};
-    ./script/docker-build.sh || exit 1;
+    ./scripts/docker-build.sh || exit 1;
     ./scripts/deploy_k8s_services.sh || exit 1;
     ./scripts/test_minimal.sh || exit 1;
 done
